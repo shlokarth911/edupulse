d0c2deab135c2840da08c46739af2fb1b7b1447c
shlokarth911 <shlokarth7@gmail.com>
2025-05-27 00:56:35 +0530

    adding new files

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectToDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose
+      .connect(process.env.MONGO_URI)
+      .then(() => console.log("Connected to DB"));
   } catch (error) {
     console.log("Failed to connect to server", error);
     process.exit(1);
